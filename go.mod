f38865f87ea1801272db1ff85b3bbd20e3c9010d
JP Meijers <gitkraken@jpmeijers.com>
2019-01-19 12:35:13 +0200

    Blur the fog

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module ttnmapper-heatmap-tile-generator
 
 require (
+	github.com/BurntSushi/graphics-go v0.0.0-20160129215708-b43f31a4a966
 	github.com/fogleman/gg v1.2.0
 	github.com/go-sql-driver/mysql v1.4.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
--- tile_fog_of_war.go
+++ tile_fog_of_war.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/graphics-go/graphics"
 	"github.com/fogleman/gg"
 	"github.com/j4/gosm"
+	"image"
+	"image/png"
 	"log"
 	"math"
+	"os"
 	"ttnmapper-heatmap-tile-generator/types"
 )
 
@@ -15,19 +19,13 @@ func drawFogOfWarTile(x int, y int, z int, entries []types.MysqlAggGridcell) {
 	tileNW19 := gosm.NewTileWithLatLong(tileNW.Lat, tileNW.Long, 19)
 
 	pixelsPer19Tile := 256 / (math.Pow(2, float64(19-z)))
-	//if pixelsPer19Tile < 1 {
-	//	log.Printf("Level19 tileNW is less than one pixel")
-	//}
+	minRadius := 10.0
+	nominalRadius := math.Max(minRadius, pixelsPer19Tile)
 
 	// fill in matrix here
 	dc := gg.NewContext(256, 256)
-	//dc.DrawRectangle(0, 0, 256, 256)
-	//dc.SetRGBA(0, 0, 0, 1)
-	//dc.Fill()
 
 	for _, entry := range entries {
-		minRadius := 3.0
-		nominalRadius := math.Max(minRadius, pixelsPer19Tile)
 
 		pixelX := float64(entry.X-tileNW19.X) * pixelsPer19Tile
 		pixelY := float64(entry.Y-tileNW19.Y) * pixelsPer19Tile
@@ -37,8 +35,6 @@ func drawFogOfWarTile(x int, y int, z int, entries []types.MysqlAggGridcell) {
 		pixelY += (nominalRadius / 2)
 
 		dc.DrawCircle(pixelX, pixelY, nominalRadius)
-		//dc.SetRGBA(1, 1, 1, 1)
-		//dc.Fill()
 	}
 
 	dc.Clip()
@@ -47,11 +43,22 @@ func drawFogOfWarTile(x int, y int, z int, entries []types.MysqlAggGridcell) {
 	dc.SetRGB(0, 0, 0)
 	dc.Fill()
 
+	srcImage := dc.Image()
+	dstImage := image.NewRGBA(srcImage.Bounds())
+	// Blur Function
+	errBlur := graphics.Blur(dstImage, srcImage, &graphics.BlurOptions{StdDev: nominalRadius / 2.0})
+	if errBlur != nil {
+		log.Print(errBlur.Error())
+	}
+
 	// Write to file
 	tilePath := fmt.Sprintf("%s/%d/%d", myConfiguration.DirFogOfWar, z, x)
 	CreateDirIfNotExist(tilePath)
 	tilePath = fmt.Sprintf("%s/%d.png", tilePath, y)
-	err := dc.SavePNG(tilePath)
+
+	newImage, _ := os.Create(tilePath)
+	defer newImage.Close()
+	err := png.Encode(newImage, dstImage)
 	if err != nil {
 		log.Print(err.Error())
 	}
